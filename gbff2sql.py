730ee0fb2bd5c3ec4bf3fd0dd9f32264313f49c5
joshrud <josh.d.rudolph@gmail.com>
2017-05-07 22:17:37 -0700

    converts genbank files to sql
    converts genbank files to insert statements that can be inputted into a sql database

--- gbff2sql.py
+++ gbff2sql.py
@@ -10,46 +10,54 @@
 
 from Bio import SeqIO #import Biopython libraries
 import sys  #for taking command-line arguments
-genome_id = "genome_id"
-replicon_id = "replicon_id"
-gene = "gene"
-locusTag = "tag"
-geneSyn = "syn"
-externalRefs = "ref"
-protein_name = "prot"
-length = 0
-accession = "accession"
-name_short="name_short"
-name_long = "name_long"
-size_bp="size_bp"
-domain="bacteria"
+
+
+
+# COUNTERS
+cntCDS = 4319   #4319 for a. tumefaciens
+gnmCDS = 0
+cntGenome = 1 
+cntReplicons = 2 #2 for a tumefaciens
+num_exons = 0
+CDSlength = 0
+geneName=""
+locusTag=""
+strand=""
+protein_product=""
+num_exons=0
 
 genbank_file = sys.argv[1] #parse input file
-    
-# print "taxID", '\t',"accession", '\t', "coordinates", '\t', "strand", '\t', "gene name",'\t', "locus_tag",'\t', "gene_synonym",'\t', "protein name",'\t', "tax ID",'\t', "EC #",'\t', "external refs"      #title 
-wanted = ["murE"] # or load all your 100 genes from a file
+
 for record in SeqIO.parse(genbank_file, "genbank"):
+    
+    if "complete genome" in record.description:
+        replicon = "Chromosome"
+    else:
+        replicon = "Plasmid"
+
+    accession = record.annotations["accessions"][0] #get accession #    
+    release_date = record.annotations["date"]       #get release date
+    structure = record.annotations["topology"]      #get replicon (linear/circular)
+    domain = record.annotations["taxonomy"][0]      #get domain
+    gnmSize = len(record)
+    assembly = record.dbxrefs[-1].split(':')[1]
+
     for f in record.features:
-        # get taxID since not within CDS
+
+        # get source information
     	if f.type == "source":
             taxID = f.qualifiers["db_xref"][0].split(':')[1]
+            genome_name = "".join(f.qualifiers["organism"])
+            if "Coli" in genome_name:
+                cntGenome=1
+            elif "tumefacien" in genome_name:
+                cntGenome=2
 
-        # look for everything within CDS region
+        # CDS information
         if f.type == "CDS" and "gene" in f.qualifiers:
-            accession = "pseudo" #initialize the accession name 
-            protein_name = "pseudo" #initialize the protein name
 
-            # check if the gene is a protein 
-            if "protein_id" in f.qualifiers:
-            	accession = "".join(f.qualifiers["protein_id"])
-            	protein_name =  "".join(f.qualifiers["product"])
-                name_long=protein_name
-                name_long.replace('"','')
-                name_long.replace("'",'')
-            # check if the gene is an enzyme
-            ecNum = "NO EC#"
-            if "EC_number" in f.qualifiers:
-                ecNum = f.qualifiers["EC_number"]
+            cntCDS+=1 #iterate genbank file counter
+            gnmCDS+=1 #iterate genome counter
 
             # assign variables from data in gbff file
             coordStart = f.location.start 
@@ -58,37 +66,71 @@ for record in SeqIO.parse(genbank_file, "genbank"):
             length = coordEnd - coordStart + 1
             size_bp=str(length)
             tempstrand = f.strand
+            locusTag = f.qualifiers.get('locus_tag')[0]
+            
+            if "gene" in f.qualifiers:
+                geneName = f.qualifiers["gene"][0]
+            else:
+                geneName = locusTag
+
             # determine if forward(+) or reverse(-) strand
             if str(tempstrand) == '1':
-                strand = '+'
+                strand = 'F'
             else:
-                strand = '-'
+                strand = 'R'
 
-            # get last few variables
-            genome_id = "genome_id"
-            replicon_id = "replicon_id"
-            gene = "".join(f.qualifiers["gene"])
-            name_short =gene
-            locusTag = "".join(f.qualifiers["locus_tag"])
-            geneSyn = "".join(f.qualifiers["gene_synonym"])
-            externalRefs = "".join(f.qualifiers["db_xref"])
+            num_exons = len(f.location.parts)
+            CDSlength = 0
+            num_exons = 0
+            for exon in f.location.parts:
+                num_exons +=1
+                # print exon entry
+                print "INSERT INTO exons (gene_id, exon, left_position, right_position, length) VALUES ("+str(cntCDS)+','+str(num_exons)+','+str(exon.start)+','+str(exon.end)+','+str(exon.end-exon.start+1)+");" 
+                CDSlength+=exon.end-exon.start+1
 
-        # print in loop: all variables just created
+            # provide default strings for accession and protein_name
+            protID = "pseudo" #initialize the accession name 
+            protein_product = "pseudo" #initialize the protein name
 
-        print "INSERT INTO genomes (tax_id,name_short, size_bp, accession) VALUES ("+str(taxID)+',\''+name_short+'\','+size_bp+',\''+accession+'\''+");" 
+            # check if the gene is a protein 
+            if "protein_id" in f.qualifiers:
+                protID = "".join(f.qualifiers["protein_id"])
+                protein_product =  "".join(f.qualifiers["product"])
 
-
-
-
-
-        # taxID, '\t', gene, '\t',protein_name, '\t', length, '\t', "bacteria", '\t',  accession 
-
-        # '\t', locusTag,  '\t', strand, '\t', exons, '\t' ,geneSyn 
-
-
+        
+            print "INSERT INTO genes (gene_id, genome_id, replicon_id, locus_tag, protein_id, name, strand, num_exons, length, product) VALUES ("+str(cntCDS)+','+str(cntGenome)+','+str(cntReplicons)+',\''+locusTag+'\',\''+protID+'\',\''+geneName+'\',\''+strand+'\','+str(num_exons)+','+str(CDSlength)+',\"'+protein_product+'\"'+");" 
 
 
+        # GENOME INFO: GENOME_ID=$cntGenome, NAME=$genome_name, TAX_ID=$tax_id, domain=$domain, num_replicons=$cntReplicons, num_genes=$gnmCDS, size_bp=$gnmSize, assembly=$assembly 
+        # REPLICONS INFO: replicon_id=$cntReplicons, genome_id=$cntGenome, name, type(chrom/plasm), shape(lin/circ), num_genes, size_bp, accession, release_date
+        # GENES INFO: gene_id, genome_id, replicon_id, locus_tag, protein_id, name, strand, num_exons, length, product
+        # EXON INFO: g
 
+
+    # print genome and replicon entries
+     
+    print "INSERT INTO replicons (replicon_id, genome_id, name, type, shape, num_genes, size_bp, accession, release_date) VALUES ("+str(cntReplicons)+','+str(cntGenome)+',\''+genome_name+'\',\''+replicon+'\',\''+structure+'\','+str(gnmCDS)+','+str(gnmSize)+',\''+accession+'\',\''+release_date+'\''+");" 
+
+    cntReplicons +=1 #iterate replicon number 
+
+print "INSERT INTO genomes (genome_id, name, tax_id, domain, num_replicons, num_genes, size_bp, assembly) VALUES ("+str(cntGenome)+',\''+genome_name+'\','+str(taxID)+',\''+ domain + '\','+str(cntReplicons)+','+str(gnmCDS)+','+str(gnmSize)+',\''+assembly+'\''+");"
+
+# get last few variables
+# genome_id = "genome_id"
+# replicon_id = "replicon_id"
+
+# gene = "".join(f.qualifiers["gene"])
+# name_short =gene
+# locusTag = "".join(f.qualifiers["locus_tag"])
+# geneSyn = "".join(f.qualifiers["gene_synonym"])
+# externalRefs = "".join(f.qualifiers["db_xref"])
+
+
+
+# check if the gene is an enzyme
+# ecNum = "NO EC#"
+# if "EC_number" in f.qualifiers:
+#     ecNum = f.qualifiers["EC_number"]
 
 
 
